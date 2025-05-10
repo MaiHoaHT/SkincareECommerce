e0caff8c86aaea1ba92c7cfab9fc0ae0178a77f1
ACME <htmaihoa792002@gmail.com>
2025-05-11 02:06:51 +0700

    add webportal template for mvc project

--- src/SkincareWeb.CustomerSite/Program.cs
+++ src/SkincareWeb.CustomerSite/Program.cs
@@ -1,7 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddControllersWithViews()
+        .AddRazorRuntimeCompilation();
+}
+else
+{
+    builder.Services.AddControllersWithViews();
+}
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+.AddCookie("Cookies")
+.AddOpenIdConnect("oidc", options =>
+{
+    options.Authority = "https://localhost:7261";  // Địa chỉ IdentityServer
+    options.ClientId = "webportal";
+    options.ClientSecret = "secret";
+    options.ResponseType = "code";
+
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("offline_access");
+    options.Scope.Add("api.skincare");
+
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.SaveTokens = true;
+    options.RequireHttpsMetadata = false;  // Tắt kiểm tra HTTPS khi chạy localhost
+    options.SignedOutRedirectUri = "https://localhost:7086";
+    options.CallbackPath = "/signin-oidc";
+});
 
 var app = builder.Build();
 
